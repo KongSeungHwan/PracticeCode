52e189682cb0bc33428b0a27b6edbf806f48993b
KongSeungHwan <102781120+KongSeungHwan@users.noreply.github.com>
2023-07-27 18:32:17 +0900

    코테 재활문제 두 수의 연산값 비교

---
+++ java/src/CodingTest/CodingTest07.java
@@ -0,0 +1,10 @@
+package CodingTest;
+
+public class CodingTest07 {
+    public static void main(String[] args) {
+        int a= 2;
+        int b= 91;
+        int val1 = Integer.parseInt(String.valueOf(a)+b);
+        int val2 = 2*a*b;
+    }
+}
