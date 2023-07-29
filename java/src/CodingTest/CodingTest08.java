04530ec41e6cb17524ce88f7cafa676d45f726d9
KongSeungHwan <102781120+KongSeungHwan@users.noreply.github.com>
2023-07-29 19:52:18 +0900

    no message

--- java/src/CodingTest/CodingTest08.java
+++ java/src/CodingTest/CodingTest08.java
@@ -5,17 +5,21 @@ import java.util.Arrays;
 public class CodingTest08 {
     public static void main(String[] args) {
         String binomial = "43 + 12";
+        int answer = 0;
         int[] term
                 = Arrays.stream(binomial
                 .replace(" ","")
-                .split("[+\\-*/]"))
+                .split("[+\\-*]"))
                 .mapToInt(Integer::parseInt)
                 .toArray(); // 항 추출
         String op= binomial.replaceAll("[0-9\\s]","");//연산자 추출
-        System.out.println(term[0]);
-        System.out.println(term[1]);
-        System.out.println(op);
-
-
+        answer=cal(term[0],term[1],op);
+    }
+    static int cal(int term1,int term2,String op){
+        switch(op){
+            case "+": return term1+term2;
+            case "-": return term1-term2;
+            default: return term1*term2;
+        }
     }
 }
