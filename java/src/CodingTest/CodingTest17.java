7896eb2ebc9541160f77404075001ac6bdd40702
KongSeungHwan <102781120+KongSeungHwan@users.noreply.github.com>
2023-08-13 13:31:57 +0900

    2023.08.13 코테 문제 연습

---
+++ java/src/CodingTest/CodingTest17.java
@@ -0,0 +1,14 @@
+package CodingTest;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class CodingTest17 {
+    public static void main(String[] args) {
+        String myString = "abstract algebra";
+        String answer = Arrays.stream(myString.split(""))
+                .map(e-> e.equals("a")? e.toUpperCase():(e.matches("^[B-Z]$"))? e.toLowerCase():e)
+                .collect(Collectors.joining());
+        System.out.println(answer);
+    }
+}
