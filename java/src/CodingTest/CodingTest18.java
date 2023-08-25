b6778ee0ac34e14499be271dfae618c6279a0d5e
KongSeungHwan <102781120+KongSeungHwan@users.noreply.github.com>
2023-08-26 01:29:31 +0900

    no message

--- java/src/CodingTest/CodingTest18.java
+++ java/src/CodingTest/CodingTest18.java
@@ -6,6 +6,6 @@ public class CodingTest18 {
     public static void main(String[] args) {
         int n= 1234;
         int answer =  Arrays.stream(String.valueOf(n).split("")).mapToInt(e->Integer.parseInt(e)).sum();
-        System.out.println(answer);
+        System.out.println(answer);////
     }
 }
