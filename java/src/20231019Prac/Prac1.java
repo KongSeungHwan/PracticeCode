37eb8ed37904b9ff9740048404ced0e6174dbcf2
KongSeungHwan <102781120+KongSeungHwan@users.noreply.github.com>
2023-11-17 23:10:36 +0900

    코테 연습 2023.11.17

---
+++ java/src/20231019Prac/Prac1.java
@@ -0,0 +1,13 @@
+package 20231019;
+public class Prac1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("나이 입력:");
+        int age = sc.nextInt();
+
+        if((age >=1)&&(age<14)){
+
+        }
+
+        }
+}
